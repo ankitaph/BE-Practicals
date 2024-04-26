bd2e357815957d901a054b6a0a19e6a185cc545c
Ankita Phatangare <91951626+ankitaph@users.noreply.github.com>
2024-04-27 00:29:14 +0530

    Update Ass3_UnionIntersection.py

--- Ass3_UnionIntersection.py
+++ Ass3_UnionIntersection.py
@@ -1,69 +1,83 @@
-A = {"x1": 0.2, "x2": 0.4, "x3": 0.6, "x4": 0.8}
-B = {"x1": 0.3, "x2": 0.5, "x3": 0.7, "x4": 0.9}
 
-R = {("x1", "y1"): 0.2, ("x1", "y2"): 0.4, ("x2", "y1"): 0.6, ("x2", "y2"): 0.8}
-S = {("x1", "y1"): 0.3, ("x1", "y2"): 0.5, ("x2", "y1"): 0.7, ("x2", "y2"): 0.9}
+import itertools
 
 
-def fuzzy_union(A, B):
+
+def fuzzy_union(set1, set2):
     union = {}
-    for key in A.keys():
-        union[key] = max(A[key], B[key])
+    for key in set1.keys():
+        union[key] = max(set1[key], set2[key])
     return union
 
-
-# Example usage
-union_result = fuzzy_union(A, B)
-print("Union:", union_result)
-
-
-def fuzzy_intersection(A, B):
-    intersection = {}
-    for key in A.keys():
-        intersection[key] = min(A[key], B[key])
-    return intersection
+def fuzzy_intersection(set1, set2):
+    intersection_set = {}
+    for key in set1.keys():
+        intersection_set[key] = min(set1[key], set2[key])
+    return intersection_set
 
 
-# Example usage
-intersection_result = fuzzy_intersection(A, B)
-print("Intersection:", intersection_result)
+def fuzzy_complement(set1):
+    complement_set = {}
+    for key in set1.keys():
+        complement_set[key] = 1 - set1[key]
+    return complement_set
 
 
-def fuzzy_complement(A):
-    complement = {}
-    for key in A.keys():
-        complement[key] = 1 - A[key]
-    return complement
+def fuzzy_difference(set1, set2):
+    difference_set = {}
+    for key in set1.keys():
+        difference_set[key] = min(set1[key], 1 - set2[key])
+    return difference_set
 
 
-# Example usage
-complement_result_A = fuzzy_complement(A)
-complement_result_B = fuzzy_complement(B)
-print("Complement A:", complement_result_A)
-print("Complement B:", complement_result_B)
+def cartesian_product_fuzzy_sets(set1, set2):
+    cartesian_product = {}
+    for key1, value1 in set1.items():
+        for key2, value2 in set2.items():
+            cartesian_product[(key1, key2)] = min(value1, value2)
+    return cartesian_product
 
 
-def fuzzy_difference(A, B):
-    difference = {}
-    for key in A.keys():
-        difference[key] = min(A[key], 1 - B[key])
-    return difference
-
-
-# Example usage
-difference_result = fuzzy_difference(A, B)
-print("Difference:", difference_result)
-
-
-def max_min_composition(R, S):
+def max_min_composition(relation1, relation2):
     composition = {}
-    for x, z in R.keys():
-        composition[(x, z)] = 0
-        for y in [key[1] for key in R.keys() if key[0] == x]:
-            composition[(x, z)] = max(composition[(x, z)], min(R[(x, y)], S[(y, z)]))
+    for (x, z1), (z2, y) in itertools.product(relation1.keys(), relation2.keys()):
+        if z1 == z2:
+            if (x, y) in composition:
+                composition[(x, y)] = max(
+                    composition[(x, y)], min(relation1[(x, z1)], relation2[(z2, y)])
+                )
+            else:
+                composition[(x, y)] = min(relation1[(x, z1)], relation2[(z2, y)])
     return composition
 
 
-# Example usage
-composition_result = max_min_composition(R, S)
-print("Max-Min Composition:", composition_result)
+# Example usage:
+
+# Define fuzzy sets
+set1 = {"x1": 0.7, "x2": 0.5}
+set2 = {"x1": 0.4, "x2": 0.6}
+
+# Operations on fuzzy sets
+union_set = fuzzy_union(set1, set2)
+intersection_set = fuzzy_intersection(set1, set2)
+complement_set1 = fuzzy_complement(set1)
+difference_set = fuzzy_difference(set1, set2)
+
+print("Union:", union_set)
+print("Intersection:", intersection_set)
+print("Complement of Set 1:", complement_set1)
+print("Difference (Set1 - Set2):", difference_set)
+
+# Create fuzzy relations
+relation1 = cartesian_product_fuzzy_sets(set1, set2)
+relation2 = cartesian_product_fuzzy_sets(set2, set1)
+
+print("relation1 : ", relation1)
+print("relation 2: ", relation2)
+
+# Max-Min Composition
+composition = max_min_composition(relation1, relation2)
+
+print("Max-Min Composition:")
+for pair, value in composition.items():
+    print(pair, ":", value)
